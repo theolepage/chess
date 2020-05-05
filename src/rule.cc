16c455ae17dfdf547f7162dc849f607e43fb86f9
Theo Lepage <theo.lepage@epita.fr>
2020-05-05 17:33:52 +0200

    fix(rule): put rule function prototypes in rule namespace and add FIXME comments in their definitions

--- src/rule.cc
+++ src/rule.cc
@@ -6,11 +6,40 @@
 namespace rule
 {
     
-    std::vector<board::Move> generate_pawn_moves(const board::Chessboard& board);
-    std::vector<board::Move> generate_king_moves(const board::Chessboard& board);
-    std::vector<board::Move> generate_bishop_moves(const board::Chessboard& board);
-    std::vector<board::Move> generate_rook_moves(const board::Chessboard& board);
-    std::vector<board::Move> generate_queen_moves(const board::Chessboard& board);
-    std::vector<board::Move> generate_knight_moves(const board::Chessboard& board);
+    std::vector<board::Move> generate_pawn_moves(const board::Chessboard&)
+    {
+        // FIXME
+        return std::vector<board::Move>();
+    }
+
+    std::vector<board::Move> generate_king_moves(const board::Chessboard&)
+    {
+        // FIXME
+        return std::vector<board::Move>();
+    }
+
+    std::vector<board::Move> generate_bishop_moves(const board::Chessboard&)
+    {
+        // FIXME
+        return std::vector<board::Move>();
+    }
+
+    std::vector<board::Move> generate_rook_moves(const board::Chessboard&)
+    {
+        // FIXME
+        return std::vector<board::Move>();
+    }
+
+    std::vector<board::Move> generate_queen_moves(const board::Chessboard&)
+    {
+        // FIXME
+        return std::vector<board::Move>();
+    }
+
+    std::vector<board::Move> generate_knight_moves(const board::Chessboard&)
+    {
+        // FIXME
+        return std::vector<board::Move>();
+    }
 
 } // namespace rule
--- src/rule.hh
+++ src/rule.hh
@@ -5,9 +5,14 @@
 #include "chessboard.hh"
 #include "move.hh"
 
-std::vector<board::Move> generate_pawn_moves(const board::Chessboard& board);
-std::vector<board::Move> generate_king_moves(const board::Chessboard& board);
-std::vector<board::Move> generate_bishop_moves(const board::Chessboard& board);
-std::vector<board::Move> generate_rook_moves(const board::Chessboard& board);
-std::vector<board::Move> generate_queen_moves(const board::Chessboard& board);
-std::vector<board::Move> generate_knight_moves(const board::Chessboard& board);
+namespace rule
+{
+
+    std::vector<board::Move> generate_pawn_moves(const board::Chessboard& board);
+    std::vector<board::Move> generate_king_moves(const board::Chessboard& board);
+    std::vector<board::Move> generate_bishop_moves(const board::Chessboard& board);
+    std::vector<board::Move> generate_rook_moves(const board::Chessboard& board);
+    std::vector<board::Move> generate_queen_moves(const board::Chessboard& board);
+    std::vector<board::Move> generate_knight_moves(const board::Chessboard& board);
+
+} // namespace rule
