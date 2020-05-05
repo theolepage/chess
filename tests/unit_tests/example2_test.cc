2c44714d68a0259e52e54c81eef873e0e06df943
nicolas.blin <nicolas.blin@epita.fr>
2020-05-05 12:58:33 +0200

    [fix][google test] fix test exemple, removed setup tear down cause their own example is not working, fixed other test so that it compile and test is green

--- tests/unit_tests/example2_test.cc
+++ tests/unit_tests/example2_test.cc
@@ -23,54 +23,26 @@ TEST(Testname, testcae)
     EXPECT_DOUBLE_EQ(0.0, 0.0);
 
     // Different
-    int val1, val2;
-    EXPECT_EQ(val1, val2); // val1 == val2
-    EXPECT_NE(val1, val2); // val1 != val2
-    EXPECT_LT(val1, val2); // val1 < val2
-    EXPECT_LE(val1, val2); // val1 <= val2
-    EXPECT_GT(val1, val2); // val1 > val2
-    EXPECT_GE(val1, val2); // val1 >= val2
+    EXPECT_EQ(1, 1); // val1 == val2
+    EXPECT_NE(1, 2); // val1 != val2
+    EXPECT_LT(1, 2); // val1 < val2
+    EXPECT_LE(1, 1); // val1 <= val2
+    EXPECT_GT(2, 1); // val1 > val2
+    EXPECT_GE(2, 2); // val1 >= val2
 
     // String
-    const char* str1, *str2;
-    std::string stdstr1, stdstr2;
-    EXPECT_STREQ(str1,str2);     //the two C strings have the same content
-    EXPECT_STRNE(str1,str2);     //the two C strings have different contents
-    EXPECT_STRCASEEQ(str1,str2); //the two C strings have the same content, ignoring case
-    EXPECT_STRCASENE(str1,str2); //the two C strings have different contents, ignoring case
-    EXPECT_EQ(stdstr1, stdstr2); //the two C++ string have the same content
+    std::string a("hello"), b("hello");
+    EXPECT_STREQ("hello","hello");     //the two C strings have the same content
+    EXPECT_STRNE("hello","Hello");     //the two C strings have different contents
+    EXPECT_STRCASEEQ("hello","Hello"); //the two C strings have the same content, ignoring case
+    EXPECT_STRCASENE("hello","helloo"); //the two C strings have different contents, ignoring case
+    EXPECT_EQ(a, b); //the two C++ string have the same content
 
     // Return status & error message
     ASSERT_EXIT(sqrt(-22.0),
                 ::testing::ExitedWithCode(1),
                 "Error: Negative Input");
 
-}
-
-// Test class with setup/teardown
-class QueueTest : public ::testing::Test
-{
-protected:
-    void SetUp() override
-    {
-        q1_.emplace(1);
-        q2_.emplace(2);
-        q2_.emplace(3);
-    }
-
-    void TearDown() override
-    {
-
-    }
-
-    std::queue<int> q0_;
-    std::queue<int> q1_;
-    std::queue<int> q2_;
-};
-
-TEST_F(QueueTest, DequeueWorks) {
-  int* n = nullptr; //q0_.pop();
-  EXPECT_EQ(n, nullptr);
 }
 
 // To start tests
