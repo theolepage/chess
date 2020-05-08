41d84794946c46878380a4fa0579d697dd978f36
pierrick.made <pierrick.made@epita.fr>
2020-05-08 03:31:25 +0200

    FIX - option-parser - fix conditions of launching pgn/perft + call listenerManager
    Signed-off-by: pierrick.made <pierrick.made@epita.fr>

--- src/parsing/option_parser/option-parser.cc
+++ src/parsing/option_parser/option-parser.cc
@@ -13,27 +13,21 @@ using namespace boost::program_options;
 
 namespace option_parser
 {
-    static void on_pgn(std::string path)
+    static void handle_dl_error(void)
     {
-        auto moves = pgn_parser::parse_pgn(path);
-    }
-
-    // Should kill program here
-    static void handle_dl_error(void) // TODO do whaaat ?
-    {
-        fprintf(stderr, "%s\n", dlerror());
+        fprintf(stderr, "%s\n", dlerror()); // TODO do whaaat ?
         exit(0);
     }
 
     static void* safe_dl_call(void* result)
     {
         if (result == nullptr)
-            handle_dl_error(); // Will kill the programm
+            handle_dl_error(); // Will kill the program
         dlerror(); // No error, need to clear the error buffer before next call
         return result;
     }
 
-    static void on_listerners(std::vector<std::string> listeners_path, listener::ListenerManager& manager)
+    static void load_listerners(std::vector<std::string> listeners_path, listener::ListenerManager& manager)
     {
         const char* symbol_name = "listener_create";
         for (const std::string& listener_path : listeners_path)
@@ -50,19 +44,14 @@ namespace option_parser
         std::cout << path << std::endl;
     }
 
-    static void start_ai(void)
-    {
-        std::cout << "The magic happens here" << std::endl;
-    }
-
     void handle_input_option(int argc, const char* argv[])
     {
-        listener::ListenerManager manager; // TODO where to put this and close it
+        listener::ListenerManager manager;
         try
         {
             std::string pgn_path, perft_path;
             std::vector<std::string> listeners_path;
-            
+
             options_description desc{"Allowed options"};
             desc.add_options()
             ("help,h", "show usage")
@@ -75,22 +64,23 @@ namespace option_parser
             notify(vm);
 
             if (vm.count("help"))
-                std::cout << desc << '\n';
-            if (vm.count("pgn"))
-                on_pgn(pgn_path);
-            if (vm.count("listeners"))
-                on_listerners(listeners_path, manager);
-            if (vm.count("perft"))
-                on_perft(perft_path);
+                std::cout << desc << '\n'; // print help message
+            else
+            {
+                if (vm.count("listeners"))
+                    load_listerners(listeners_path, manager);
+                if (vm.count("pgn"))
+                    manager.play_pgn_moves(pgn_parser::parse_pgn(pgn_path));
+                else if (vm.count("perft"))
+                    on_perft(perft_path);
+                else
+                    manager.play_ai();
+            }
         }
         catch (const error &ex)
         {
             //TODO triggered on bad option, what expected ?
             std::cerr << ex.what() << '\n';
-        }
-        if (argc == 1) // No option
-        {
-            start_ai();
         }
         manager.close_listeners();
     }
