34e28797ace75d0f9f037521626e3ea28f774b2d
Alan Leblanc <alan.leblanc@epita.fr>
2020-05-13 20:40:00 +0000

    Update evaluation.hh

--- src/chess_engine/ai/evaluation.hh
+++ src/chess_engine/ai/evaluation.hh
@@ -18,7 +18,8 @@ namespace ai
     // They should be symetrically accessed
     // (as if the ranks where reversed) for white pieces
     constexpr piece_square_table_t piece_square_table_queen = {
-    -20,-10,-10, -5, -5,-10,-10,-20, -10,  0,  0,  0,  0,  0,  0,-10,
+    -20,-10,-10, -5, -5,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
     -10,  0,  5,  5,  5,  5,  0,-10,
     -5,  0,  5,  5,  5,  5,  0, -5,
     0,  0,  5,  5,  5,  5,  0, -5,
