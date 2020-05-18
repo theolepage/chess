c3f6ec7ae0292abe520a0d7c3e3d8e9b0d6bd217
Alan Leblanc <alan.leblanc@epita.fr>
2020-05-18 16:24:38 +0200

    add(evaluation-test): add winner comparison

--- tests/eval-evaluation.py
+++ tests/eval-evaluation.py
@@ -41,40 +41,63 @@ def index_by_key(list, elt, key=identity):
 
     return list_with_key_applied.index(elt)
 
-def fens_have_same_order(fen_couple, fen_and_value_couples_1, fen_and_value_couples_2):
+def get_choice_dissonance(fen_couple, fen_and_ref_value_couples, fen_and_value_couples):
     first_fen, second_fen = fen_couple
 
-    first_fen_index_1 = index_by_key(fen_and_value_couples_1, first_fen, key=first)
-    second_fen_index_1 = index_by_key(fen_and_value_couples_1, second_fen, key=first)
+    first_fen_ref_index = index_by_key(fen_and_ref_value_couples, first_fen, key=first)
+    second_fen_ref_index = index_by_key(fen_and_ref_value_couples, second_fen, key=first)
 
-    first_fen_index_2 = index_by_key(fen_and_value_couples_2, first_fen, key=first)
-    second_fen_index_2 = index_by_key(fen_and_value_couples_2, second_fen, key=first)
+    first_fen_index = index_by_key(fen_and_value_couples, first_fen, key=first)
+    second_fen_index = index_by_key(fen_and_value_couples, second_fen, key=first)
 
-    return (first_fen_index_1 <= second_fen_index_1 and first_fen_index_2 <= second_fen_index_2) \
-        or (second_fen_index_1 <= first_fen_index_1 and second_fen_index_2 <= first_fen_index_2)
+    first_fen_ref_value = second(fen_and_ref_value_couples[first_fen_ref_index])
+    second_fen_ref_value = second(fen_and_ref_value_couples[second_fen_ref_index])
+
+    first_fen_value = second(fen_and_value_couples[first_fen_index])
+    second_fen_value = second(fen_and_value_couples[second_fen_index])
+
+    if (first_fen_ref_index <= second_fen_ref_index and first_fen_index <= second_fen_index) \
+       or (second_fen_ref_index <= first_fen_ref_index and second_fen_index <= first_fen_index):
+        return None
+
+    return {'fen_string_1': first_fen,
+            'fen_string_1_ref_value': first_fen_ref_value,
+            'fen_string_1_value': first_fen_value,
+            'fen_string_2': second_fen,
+            'fen_string_2_ref_value': second_fen_ref_value,
+            'fen_string_2_value': second_fen_value}
+
+def get_winner_dissonance(fen, fen_and_ref_value_couples, fen_and_value_couples):
+    fen_ref_index = index_by_key(fen_and_ref_value_couples, fen, key=first)
+    fen_index = index_by_key(fen_and_value_couples, fen, key=first)
+
+    fen_ref_value = second(fen_and_ref_value_couples[fen_ref_index])
+    fen_value = second(fen_and_value_couples[fen_index])
+
+    if (fen_ref_value <= 0 and fen_value <= 0) or (fen_ref_value > 0 and fen_value > 0):
+        return None
+
+    return {'fen_string': fen,
+            'fen_string_ref_value': fen_ref_value,
+            'fen_string_value': fen_value}
+
 
 # Assumes that the couples are sorted by value
 def dissonances(fen_and_ref_value_couples, fen_and_value_couples):
-    dissonances = []
+    winner_dissonances = []
+    choice_dissonances = []
+
+    for fen in fen_strings:
+        winner_dissonance = get_winner_dissonance(fen, fen_and_ref_value_couples, fen_and_value_couples)
+        if winner_dissonance:
+            winner_dissonances.append(winner_dissonance)
 
     for fen_couple in n_choose_2(fen_strings):
-        if not fens_have_same_order(fen_couple, fen_and_ref_value_couples, fen_and_value_couples):
-            first_fen, second_fen = fen_couple
+        choice_dissonance = get_choice_dissonance(fen_couple, fen_and_ref_value_couples, fen_and_value_couples)
+        if choice_dissonance:
+            choice_dissonances.append(choice_dissonance)
 
-            first_fen_ref_index = index_by_key(fen_and_ref_value_couples, first_fen, key=first)
-            second_fen_ref_index = index_by_key(fen_and_ref_value_couples, second_fen, key=first)
-
-            first_fen_index = index_by_key(fen_and_value_couples, first_fen, key=first)
-            second_fen_index = index_by_key(fen_and_value_couples, second_fen, key=first)
-
-            dissonances.append({'fen_string_1': first_fen,
-                                'fen_string_1_ref_value': second(fen_and_ref_value_couples[first_fen_ref_index]),
-                                'fen_string_1_value': second(fen_and_value_couples[first_fen_index]),
-                                'fen_string_2': second_fen,
-                                'fen_string_2_ref_value': second(fen_and_ref_value_couples[second_fen_ref_index]),
-                                'fen_string_2_value': second(fen_and_value_couples[second_fen_index])})
-
-    return dissonances
+    return {'winner_dissonances': winner_dissonances, 'choice_dissonances': choice_dissonances}
 
 def evaluate_eval_fun(eval_fun):
     fen_and_ref_value_couples = [(fen, ref_eval(fen)) for fen in fen_strings]
@@ -87,8 +110,6 @@ def evaluate_eval_fun(eval_fun):
     print(fen_and_ref_value_couples)
     print(fen_and_value_couples)
 
-    dissonance_list = dissonances(fen_and_ref_value_couples, fen_and_value_couples)
-
-    return (len(dissonance_list), dissonance_list)
+    return dissonances(fen_and_ref_value_couples, fen_and_value_couples)
 
 print(evaluate_eval_fun(eval))
