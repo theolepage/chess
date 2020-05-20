d93a2f45d17bd6862e166fbba6d1de59579ed20d
Alan Leblanc <alan.leblanc@epita.fr>
2020-05-20 17:51:44 +0200

    add(evaluation): add material evaluation tests

--- tests/unit_tests/evaluate_test.cc
+++ tests/unit_tests/evaluate_test.cc
@@ -52,6 +52,20 @@ TEST(EvaluateMaterial, SimpleRook)
     EXPECT_EQ(evaluate_material(black_board), -abs_board_value);
 }
 
+TEST(EvaluateMaterial, InitialBoard)
+{
+    Chessboard board;
+
+    EXPECT_EQ(evaluate_material(board), 0);
+}
+
+TEST(EvaluateMaterial, MultiplePieces)
+{
+    Chessboard board = Chessboard("2k5/7R/1q1p1p2/8/4N3/8/2B5/8");
+
+    EXPECT_EQ(evaluate_material(board), 500 + 330 + 320 - 20000 - 100 * 2 - 900);
+}
+
 TEST(EvaluateSquares, SimplePawn)
 {
     Chessboard white_board = Chessboard("8/1P6/8/8/8/8/8/8");
